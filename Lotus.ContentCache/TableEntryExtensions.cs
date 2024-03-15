45ba95f86ec97acece53b04c5ebb230475b6f27c
Ada <ada@chronovore.dev>
2024-03-15 14:38:50 +0000

    drop BitPacked

--- Lotus.ContentCache/TableEntryExtensions.cs
+++ Lotus.ContentCache/TableEntryExtensions.cs
@@ -5,8 +5,8 @@ using System.Buffers;
 using System.Buffers.Binary;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
 using DragonLib.Compression;
-using DragonLib.IO;
 using Lotus.ContentCache.IO;
 using Lotus.ContentCache.Types;
 
@@ -28,9 +28,11 @@ public static class TableEntryExtensions {
         using var file = new PooledMemoryMarshal(entry.Size, false);
         while (reader.Cursor < entry.CompressedSize) {
             if (reader.Peek<byte>() >> 7 == 1) {
-                var frameHeader = BitPacked.Unpack<BlockFrameHeader>(BinaryPrimitives.ReverseEndianness(reader.Read<ulong>()));
+                var frameHeaderRaw = BinaryPrimitives.ReverseEndianness(reader.Read<ulong>());
+                var frameHeader = Unsafe.As<ulong, BlockFrameHeader>(ref frameHeaderRaw);
                 Debug.Assert(frameHeader.UseNewFormat, "frameHeader.UseNewFormat");
-                Debug.Assert(frameHeader.Unknown1 == 1, "frameHeader.Unknown1 == 1");
+                Debug.Assert(frameHeader.CompressionType == BlockCompressionType.Oodle, "frameHeader.CompressionType == BlockCompressionType.Oodle");
+                Debug.Assert(frameHeader.BlockSize <= 0x40000, "frameHeader.BlockSize <= 0x40000");
                 var buffer = reader.Slice(frameHeader.CompressedSize);
                 if (!frameHeader.IsCompressed) {
                     file.Paste(buffer);
---
+++ Lotus.ContentCache/Types/BlockCompressionType.cs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MPL-2.0
+
+namespace Lotus.ContentCache.Types;
+
+public enum BlockCompressionType : byte {
+    Oodle = 1,
+}
